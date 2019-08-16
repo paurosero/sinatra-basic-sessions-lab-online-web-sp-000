4eba965cf89c98b540716620311368ec9ea0535a
Paul Rosero <paul_rosero@yahoo.com>
2019-08-16 00:58:30 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  configure do
+    enable :sessions
+    set :session_secret, "paris"
+  end
+  
+  get '/' do
+   
+    erb :index
+  en
 end
